903c01bb03168d42fc34e310192db424fd5e03b9
Ondřej Krpec <ondrej.krpec@gmail.com>
2018-03-10 08:49:53 +0100

    2018-03-10 created project's skeleton

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Remote-API-Web-Reference-for-Java-Enterprise-Applications
+
+Install:
+v top adresari staci mvn clean install
+
+Spusteni
+BACKEND:
+v backend adresari mvn spring-boot:run
+
+FRONTEND:
+v frontend/src/main/frontend npm start
---
+++ resttest-app/backend/src/main/java/com/resttest/resttestapp/ResttestAppApplication.java
@@ -0,0 +1,12 @@
+package com.resttest.resttestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResttestAppApplication.class, args);
+	}
+}
